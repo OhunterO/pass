6e2f74551d171f83c9672e46853deb7a171ca967
Administrator <Administrator@IQZIUD5SMKV6Q1Q>
2017-01-22 12:48:57 +0800

    test

--- src/main/java/com/sl/one/controller/login/LoginController.java
+++ src/main/java/com/sl/one/controller/login/LoginController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Created by shi on 2017/1/7.
- */
 @Controller
 @RequestMapping("/login")
 public class LoginController {
